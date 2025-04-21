97b74b2c03910aa7faf17eae1314a1fb33eae23f
Mayank Piparde <mayankpiparde20@gmail.com>
2025-04-21 22:13:05 +0530

    Question6

---
+++ 01_basics/06_swap.js
@@ -0,0 +1,9 @@
+let num1 = 5;
+let num2 = 10;
+console.log(`Previous num1: ${num1}`);
+console.log(`Previous num2: ${num2}`);
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+console.log(`Current num1: ${num1}`);
+console.log(`Current num2: ${num2}`);
