80cf9fb1ec05badf5117456ef65f65c83ebb9833
Mayank Piparde <mayankpiparde20@gmail.com>
2025-04-28 18:56:39 +0530

    Question17

---
+++ 01_basics/17_sorting&length.js
@@ -0,0 +1,6 @@
+let arr = [54, 86, 17, 9, 43, 8, 22, 62, 36, 94, 65, 1, 54, 12, 5];
+
+sortedarr = arr.sort((a, b) => a - b);
+console.log(`Shorted array is : ${sortedarr}`);
+
+console.log(`Length of array is : ${sortedarr.length}`);
